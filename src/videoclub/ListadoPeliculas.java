3300a5e013660af754a6b899a3899339f6601c3d
rpachotome <rpachotome@eniac32.danielcastelao.org>
2015-03-19 11:38:54 +0100

    Avanzamos

--- src/videoclub/ListadoPeliculas.java
+++ src/videoclub/ListadoPeliculas.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -11,23 +12,25 @@ import java.util.Scanner;
  */
 public class ListadoPeliculas {
 
+    ArrayList<Peliculas> fPeliculas = new ArrayList<Peliculas>();
+
     public void ListadoPeliculas() {
-        ArrayList<Peliculas> fPeliculas = new ArrayList<Peliculas>();
+
         File fichero = new File("Lista.txt");
         Scanner scan = null;
-        Peliculas listaPeliculas = new Peliculas();
 
         try {
             scan = new Scanner(fichero);
             while (scan.hasNextLine()) {
                 String linea = scan.nextLine();
                 String[] dividirLista = linea.split(", ");
-
+                Peliculas listaPeliculas = new Peliculas();
                 listaPeliculas.setTitulo(dividirLista[0]);
                 listaPeliculas.setDirector(dividirLista[1]);
                 listaPeliculas.setGenero(dividirLista[2]);
                 listaPeliculas.setAño(dividirLista[3]);
-                //listaPeliculas.setCantidad(Integer.parseInt(dividirLista[4]));
+                listaPeliculas.setCantidad(Integer.parseInt(dividirLista[4]));
+                fPeliculas.add(listaPeliculas);
             }
         } catch (Exception except1) {
             except1.printStackTrace();
@@ -41,15 +44,53 @@ public class ListadoPeliculas {
             }
         }
 
-        //public void mostrarPeliculas() {
-        Iterator<Peliculas> itrPeliculas = fPeliculas.iterator();
-        while (itrPeliculas.hasNext()) {
-            Peliculas partido = itrPeliculas.next();
-            System.out.println(listaPeliculas.getTitulo()+" "
-                    + listaPeliculas.getDirector() + "-"
-                    + listaPeliculas.getGenero() + " "
-                    + listaPeliculas.getAño());
+        /*System.out.println("... Guardados "+fPeliculas.size()+" partidos de fútbol  ...");
+         System.out.println("ME cago en mi madre");
+         Iterator<Peliculas> itrPeli = fPeliculas.iterator();
+         while(itrPeli.hasNext()){
+         Peliculas pelis = itrPeli.next();
+         System.out.println(pelis.getTitulo() + " "
+         + pelis.getDirector() + "-"
+         + pelis.getGenero() + " "
+         + pelis.getAño());
+         }*/
+    }
+
+    public int Menu() {
+        int opcion = JOptionPane.showOptionDialog(null, "Selecciona una opción",
+                "Menu", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE,
+                null, new Object[]{"Buscar pelicula", "Devolver pelicula", "Salir del Videoclub"},
+                "Sair do programa");
+        return opcion;
+    }
+
+    public void BuscarPelicula() {
+        int opcion = JOptionPane.showOptionDialog(null, "Selecciona una opción",
+                "Menu", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE,
+                null, new Object[]{"Buscar pelicula por titulo",
+                    "Buscar pelicula por director",
+                    "Buscar pelicula por genero", 
+                    "Buscar pelicula por año", 
+                    "Salir del Videoclub"},
+                "Sair do programa");
+        Iterator<Peliculas> itrPeli = fPeliculas.iterator();
+         while(itrPeli.hasNext()){
+        
+        switch(opcion){
+            case 0:
+                String a=JOptionPane.showInputDialog("Introduzca el titulo de la pelicula");
+                 Peliculas pelis = itrPeli.next();
+                if (a.equalsIgnoreCase(pelis.getTitulo())){
+               int seleccion = JOptionPane.showOptionDialog(null, pelis.toString(),
+                "Menu", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE,
+                null, new Object[]{"Alquilar",
+                "Volver",},
+                "Sair do programa");
+                    
+                } break;
         }
-    
-}
+
+    }
+
+    }
 }
